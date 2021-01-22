8e966bcb83f71a216b6f132ba602f9fc5b59d33d
hYdos <haydenv06@gmail.com>
2021-01-22 16:28:04 +1000

    fix chat handling

--- src/main/java/net/swifthq/swiftapi/chat/BasicChatHandler.java
+++ src/main/java/net/swifthq/swiftapi/chat/BasicChatHandler.java
@@ -1,11 +1,14 @@
 package net.swifthq.swiftapi.chat;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
 
 public class BasicChatHandler implements ChatHandler {
     @Override
-    public String onChatMessage(String message, ServerPlayerEntity player) {
-        return player.getName().asString() + Formatting.DARK_GRAY + " » " + Formatting.RESET + message.replace("&", "\u00A7");
+    public String onChatMessage(String vanilla, ServerPlayerEntity player) {
+        String message = player.getName().asString() + Formatting.DARK_GRAY + " » " + Formatting.RESET + vanilla.replace("&", "\u00A7");
+        player.server.getPlayerManager().broadcastChatMessage(new LiteralText(message), false);
+        return message;
     }
 }
